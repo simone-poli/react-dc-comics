8b1eb5ef442657d35981e009ac6783a688f4f69d
simone-poli <polismn97@gmail.com>
2025-07-14 16:24:23 +0200

    Add Bootstrap integration and restructure App component with new Header; remove unused assets and styles

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+    return (
+        <header>
+        <div className="container d-flex justify-content-between align-items-center p-3">
+            <div>
+                <img src="../public/img/dc-logo.png" alt="Logo DC" />
+            </div>
+            <div className="d-flex gap-3">
+                <a href="">characters</a>
+                <a href="" className="active">comics</a>
+                <a href="">movies</a>
+                <a href="">tv</a>
+                <a href="">games</a>
+                <a href="">collectibles</a>
+                <a href="">videos</a>
+                <a href="">fans</a>
+                <a href="">news</a>
+                <a href="">shop</a>
+            </div>
+        </div>
+        </header>
+    )
+}
